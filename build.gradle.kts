b1a1c7496c240cca417015f74fd8eb73faaaebf9
Tomáš "Wega" Weglarz <82312488+ThomasWega@users.noreply.github.com>
2024-11-08 12:08:48 +0100

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    application
     id("io.freefair.lombok") version "8.10.2"
     id("java")
 }
@@ -14,6 +15,10 @@ dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "me.weglarz.Main"
 }
 
 tasks.test {
